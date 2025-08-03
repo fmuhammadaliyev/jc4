9eec988ce5eff242a3dfedd2f4177782ef302a7a
muhammadaliyev <muhammadaliyev>
2025-08-03 13:59:38 +0500

    fahriddin7

---
+++ js/app.js
@@ -0,0 +1,217 @@
+//========================================
+//------------------------------------------
+//------------------------------------------
+//Function declaration
+//1-misol
+function showAgeD(age) {
+  console.log(`${age}`);
+}
+showAgeD(20);
+//----------------------------------------
+
+//2-misol
+function addNumbersD(a, b) {
+  console.log(`${a},${b}`);
+}
+addNumbersD(3, 7);
+//----------------------------------------
+
+//3-misol
+function greetD(name) {
+  console.log(`${name}`);
+}
+greetD("Mirzo");
+//----------------------------------------
+
+//4-misol
+function fullNameD(firsName, LastName) {
+  console.log(`${firsName},${LastName}`);
+}
+fullNameD("Ali", "Valiyev");
+//----------------------------------------
+
+//5-misol
+function rectanglePerimeterD(lenght, width) {
+  console.log(`${lenght}, ${width}`);
+}
+rectanglePerimeterD(5, 3);
+//----------------------------------------
+
+//6-misol
+function triangleSidesD(a, b, c) {
+  console.log(`${a}, ${b}, ${c}`);
+}
+triangleSidesD(3, 4, 5);
+//----------------------------------------
+
+//7-misol
+function squareNumberD(number) {
+  console.log(`${number}`);
+}
+squareNumberD(5);
+//----------------------------------------
+
+//8-misol
+function animalInfoD(name, age) {
+  console.log(`${name} ${age}`);
+}
+animalInfoD("mushuk", 5);
+//----------------------------------------
+
+//9-misol
+function sayhelloD(hello) {
+  console.log(`${hello}`);
+}
+sayhelloD("salom dunyo");
+//----------------------------------------
+
+//10-misol
+function printnameD(naem1, name2, name3, name4, name5) {
+  console.log(`${naem1} ${name2}${name3} ${name4}${name5}`);
+}
+printnameD("Ali", "Vali", "Gulbahor", "Zilola", "Javohir");
+
+//========================================
+//------------------------------------------
+//------------------------------------------
+//Function Expression
+// 1-misol
+const showAgeE = function (age) {
+  console.log(`${age}`);
+};
+showAgeE(20);
+//------------------------------------------
+
+// 2-misol
+const addNumbersE = function (a, b) {
+  console.log(`${a},${b}`);
+};
+addNumbersE(3, 7);
+//------------------------------------------
+
+// 3-misol
+const greetE = function (name) {
+  console.log(`${name}`);
+};
+greetE("Mirzo");
+//------------------------------------------
+
+// 4-misol
+const fullNameE = function (firsName, LastName) {
+  console.log(`${firsName},${LastName}`);
+};
+fullNameE("Ali", "Valiyev");
+//------------------------------------------
+
+// 5-misol
+const rectanglePerimeterE = function (lenght, width) {
+  console.log(`${lenght}, ${width}`);
+};
+rectanglePerimeterE(5, 3);
+//------------------------------------------
+
+// 6-misol
+const triangleSidesE = function (a, b, c) {
+  console.log(`${a}, ${b}, ${c}`);
+};
+triangleSidesE(3, 4, 5);
+//------------------------------------------
+
+// 7-misol
+const squareNumberE = function (number) {
+  console.log(`${number}`);
+};
+squareNumberE(5);
+//------------------------------------------
+
+// 8-misol
+const animalInfoE = function (name, age) {
+  console.log(`${name} ${age}`);
+};
+animalInfoE("mushuk", 5);
+//------------------------------------------
+
+// 9-misol
+const sayhelloE = function (hello) {
+  console.log(`${hello}`);
+};
+sayhelloE("salom dunyo");
+//------------------------------------------
+
+// 10-misol
+const printnameE = function (naem1, name2, name3, name4, name5) {
+  console.log(`${naem1} ${name2}${name3} ${name4}${name5}`);
+};
+printnameE("Ali", "Vali", "Gulbahor", "Zilola", "Javohir");
+//========================================
+//------------------------------------------
+//------------------------------------------
+//Arrow Function
+// 1-misol
+const showAgeA = (age) => {
+  console.log(`${age}`);
+};
+showAgeA(20);
+//------------------------------------------
+
+// 2-misol
+const addNumbersA = (a, b) => {
+  console.log(`${a},${b}`);
+};
+addNumbersA(3, 7);
+//------------------------------------------
+
+// 3-misol
+const greetA = (name) => {
+  console.log(`${name}`);
+};
+greetA("Mirzo");
+//------------------------------------------
+
+// 4-misol
+const fullNameA = (firsName, LastName) => {
+  console.log(`${firsName},${LastName}`);
+};
+fullNameA("Ali", "Valiyev");
+//------------------------------------------
+
+// 5-misol
+const rectanglePerimeterA = (lenght, width) => {
+  console.log(`${lenght}, ${width}`);
+};
+rectanglePerimeterA(5, 3);
+//------------------------------------------
+
+// 6-misol
+const triangleSidesA = (a, b, c) => {
+  console.log(`${a}, ${b}, ${c}`);
+};
+triangleSidesA(3, 4, 5);
+//------------------------------------------
+
+// 7-misol
+const squareNumberA = (number) => {
+  console.log(`${number}`);
+};
+squareNumberA(5);
+//------------------------------------------
+
+// 8-misol
+const animalInfoA = (name, age) => {
+  console.log(`${name} ${age}`);
+};
+animalInfoA("mushuk", 5);
+//------------------------------------------
+
+// 9-misol
+const sayhelloA = (hello) => {
+  console.log(`${hello}`);
+};
+sayhelloA("salom dunyo");
+//------------------------------------------
+
+// 10-misol
+const printnameA = (naem1, name2, name3, name4, name5) => {
+  console.log(`${naem1} ${name2}${name3} ${name4}${name5}`);
+};
+printnameA("Ali", "Vali", "Gulbahor", "Zilola", "Javohir");
